4e33a0a48c40c65693da31893ea1ba6e920bf694
Flavio1200 <flavio.r.1200@gmail.com>
2026-06-28 12:37:04 -0400

    actualizada api paises

--- app/composables/apiCountries.js
+++ app/composables/apiCountries.js
@@ -1,31 +1,7 @@
 
 export const useCountriesAPI = () => {
-  const apiKey = 'rc_live_ea352ed3c2be456d86b4395150cd0ae9'
-  const baseUrl = 'https://api.restcountries.com/countries/v5'
-  const responseFields = 'names.common,codes.alpha_3,capitals,population,region,flag.url_png'
-
-  const normalizeCountry = (country) => {
-    const capitalName = Array.isArray(country.capitals) && country.capitals.length > 0
-      ? country.capitals[0].name || country.capitals[0]
-      : ''
-
-    return {
-      name: { common: country['names.common'] || country.names?.common || '' },
-      capital: capitalName ? [capitalName] : [],
-      population: country.population || 0,
-      region: country.region || '',
-      flags: { png: country['flag.url_png'] || country.flag?.url_png || '' },
-      cca3: country['codes.alpha_3'] || country.codes?.alpha_3 || ''
-    }
-  }
-
   const getCountries = async () => {
-    const response = await $fetch(`${baseUrl}?response_fields=${responseFields}&limit=100`, {
-      headers: { Authorization: `Bearer ${apiKey}` }
-    })
-
-    const objects = response?.data?.objects || []
-    return objects.map(normalizeCountry)
+    return $fetch('/api/countries')
   }
 
   const getCountryByName = async (name) => {
@@ -33,12 +9,7 @@ export const useCountriesAPI = () => {
       return []
     }
 
-    const response = await $fetch(`${baseUrl}/names.common?q=${encodeURIComponent(name)}&response_fields=${responseFields}&limit=100`, {
-      headers: { Authorization: `Bearer ${apiKey}` }
-    })
-
-    const objects = response?.data?.objects || []
-    return objects.map(normalizeCountry)
+    return $fetch(`/api/countries?name=${encodeURIComponent(name)}`)
   }
 
   return { getCountries, getCountryByName }
---
+++ server/api/countries.get.ts
@@ -0,0 +1,43 @@
+import { defineEventHandler, getQuery } from 'h3'
+
+const apiKey = 'rc_live_ea352ed3c2be456d86b4395150cd0ae9'
+const baseUrl = 'https://api.restcountries.com/countries/v5'
+const responseFields = 'names.common,codes.alpha_3,capitals,population,region,flag.url_png'
+
+const normalizeCountry = (country) => {
+  const capitalName = Array.isArray(country.capitals) && country.capitals.length > 0
+    ? country.capitals[0].name || country.capitals[0]
+    : ''
+
+  return {
+    name: { common: country['names.common'] || country.names?.common || '' },
+    capital: capitalName ? [capitalName] : [],
+    population: country.population || 0,
+    region: country.region || '',
+    flags: { png: country['flag.url_png'] || country.flag?.url_png || '' },
+    cca3: country['codes.alpha_3'] || country.codes?.alpha_3 || ''
+  }
+}
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+  const name = query.name?.toString()
+
+  try {
+    const url = name
+      ? `${baseUrl}/names.common?q=${encodeURIComponent(name)}&response_fields=${responseFields}&limit=100`
+      : `${baseUrl}?response_fields=${responseFields}&limit=100`
+
+    const response = await $fetch(url, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    })
+
+    const objects = response?.data?.objects || []
+    return objects.map(normalizeCountry)
+  } catch (error) {
+    console.error('Countries API error:', error)
+    return []
+  }
+})
